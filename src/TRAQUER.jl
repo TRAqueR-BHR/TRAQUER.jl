8c07313de284cc6baff403e80f7fddde39b1ae09
Medi Rossetti <medirossetti@yahoo.fr>
2021-03-09 17:00:13 +0100

    add dependencies

--- src/TRAQUER.jl
+++ src/TRAQUER.jl
@@ -1,5 +1,53 @@
 module TRAQUER
 
-greet() = print("Hello World!")
+greet() = print("Lorembv")
+
+module Enums
+    include("Enums/enums.jl")
+end  # module Enums
+
+module TRAQUERUtils
+
+    include("./utils/utils.jl")
+
+end # module TRAQUERUtils
+
+
+
+module Model
+    export Patient, FctUnit, FctUnitStay
+    using PostgresqlDAO,PostgresqlDAO.Model,TimeZones,..Enums.Gender
+    include("Model/abstract-types.jl")
+    include("Model/FctUnit.jl")
+    include("Model/FctUnitStay.jl")
+    include("Model/Patient.jl")
+
+
+
+
+end  # module Model
+
+module DAO
+
+    module FctUnitDAO
+        using ...Model,PostgresqlDAO,PostgresqlDAO.Model.Enums.CRUDType,..DAO
+        include("DAO/FctUnitDAO.jl")
+    end  # module
+
+    module FctUnitStayDAO
+        using ...Model,PostgresqlDAO,PostgresqlDAO.Model.Enums.CRUDType,..DAO
+        include("DAO/FctUnitStayDAO.jl")
+    end  # module
+
+    module PatientDAO
+        using ...Model,PostgresqlDAO,PostgresqlDAO.Model.Enums.CRUDType,..DAO
+        include("DAO/PatientDAO.jl")
+    end  # module
+
+
+
+
+end  # module DAO
+
 
 end # module
