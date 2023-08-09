dc6b0972a599845643558c0ccb0d5eb72ceda812
Vincent Laugier <vincent.laugier@gmail.com>
2023-08-09 04:37:08 +0000

    remove deprecated functions for converting the request and result type

--- custom/cht-noumea/src/cht-noumea-def.jl
+++ custom/cht-noumea/src/cht-noumea-def.jl
@@ -1,8 +1,6 @@
 function importStays end
 function importAnalyses end
 function checkIfNotAtRiskAnymore end
-function convertStringInInputFileToANALYSIS_REQUEST_TYPE end
-function convertStringInInputFileToANALYSIS_RESULT_VALUE_TYPE end
 function convertETLInputDataToRequestAndResultType end
 function getBasicInformationAboutAnalysesInputFile end
 function getBasicInformationAboutStaysInputFile end
