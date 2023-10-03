b0d67be988e3387c9d378b3664cc84275ecc9f13
Vincent Laugier <vincent.laugier@gmail.com>
2023-10-03 23:46:55 +0000

    add stay/save-patient-isolation-date-from-event-requiring-attention

--- scripts/web/api-def/stay-api.jl
+++ scripts/web/api-def/stay-api.jl
@@ -470,3 +470,100 @@ new_route = route("/api/stay/listing", req -> begin
 
 end)
 api_routes = (api_routes..., new_route) # append the route
+
+
+#
+# Save patient isolation date of a stay that best matches the event requiring attention
+#
+new_route = route("/api/stay/save-patient-isolation-date-from-event-requiring-attention", req -> begin
+
+    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    if req[:method] == "OPTIONS"
+        return(respFor_OPTIONS_req())
+    end
+
+    @info "API /api/stay/save-patient-isolation-date-from-event-requiring-attention"
+
+    # Check if the user is allowed
+    status_code = TRAQUERUtil.initialize_http_response_status_code(req)
+
+    if status_code != 200
+        return Dict(:body => String(JSON.json(missing)),
+                    :headers => Dict("Content-Type" => "text/plain",
+                                      "Access-Control-Allow-Origin" => "*"),
+                    :status => status_code
+                     )
+    end
+
+
+    #
+    # Heart of the API
+    #
+
+    # Initialize results
+    error = nothing
+    stay::Union{Missing,Stay} = missing
+
+    status_code = try
+
+
+        cryptPwd = TRAQUERUtil.extractCryptPwdFromHTTPRequest(req)
+
+        obj = JSON.parse(String(req[:data]))
+        obj = PostgresORM.PostgresORMUtil.dictnothingvalues2missing(obj)
+
+        eventRequiringAttention = json2entity(EventRequiringAttention, obj["event"])
+        isolationTime = TRAQUERUtil.browserDateString2ZonedDateTime(
+            obj["isolationTime"], TRAQUERUtil.getTimeZone()
+        )
+
+        # Get the user as extracted from the JWT
+        appuser = req[:params][:appuser]
+
+        TRAQUERUtil.executeOnBgThread() do
+            stay = TRAQUERUtil.createDBConnAndExecute() do dbconn
+                return StayCtrl.saveIsolationTime(
+                    eventRequiringAttention,
+                    isolationTime,
+                    dbconn
+                )
+            end
+        end
+
+        200 # status code
+
+    catch e
+        formatExceptionAndStackTrace(e,
+                                     stacktrace(catch_backtrace()))
+        # rethrow(e) # Do not rethrow the error because we do want to send a
+                     #  custom design if the file could not be retrieved
+        error = e
+        500 # status_code
+
+    end
+
+    #
+    # Prepare the result
+    #
+    result::Union{Missing,String} = missing
+    try
+        if status_code == 200
+            result = String(JSON.json(stay)) # The client side doesn't really need the message
+        else
+            result = String(JSON.json(string(error)))
+        end
+    catch e
+        formatExceptionAndStackTrace(e,
+                                     stacktrace(catch_backtrace()))
+        rethrow(e)
+    end
+
+    # Send the result
+    Dict(:body => result,
+         :headers => Dict("Content-Type" => "application/json",
+                          "Access-Control-Allow-Origin" => "*"),
+         :status => status_code
+        )
+
+end)
+api_routes = (api_routes..., new_route) # append the route
