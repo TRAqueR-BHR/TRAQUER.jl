4a1aec9b660188efa0b47c8734c3d20d6b28335e
Vincent Laugier <vincent.laugier@gmail.com>
2023-10-04 00:01:27 +0000

    add comment

--- src/Controller/ContactExposureCtrl/upsert.jl
+++ src/Controller/ContactExposureCtrl/upsert.jl
@@ -3,7 +3,9 @@ function ContactExposureCtrl.upsert!(contactExposure::ContactExposure, dbconn::L
     # Check whether an exposure already exists,
     # NOTE: When looking for an existing exposure, we dont use the outbreak in the filter
     #       so that we dont create a duplicate exposure (this can happen when an infectious
-    #       status is associated to several outbreaks)
+    #       status is associated to several outbreaks). This is not perfect, in the scenario
+    #       where we integrate some stays late and that are more supposed to be associated
+    #       with the first outbreak, those will be associated to the most recent outbreak
     filterObject = ContactExposure(
         contact = contactExposure.contact,
         carrier = contactExposure.carrier,
