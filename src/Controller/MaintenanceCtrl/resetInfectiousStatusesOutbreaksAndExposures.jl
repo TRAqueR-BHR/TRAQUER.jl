621845dda945ddc886fa615d0ce8758ab123ad90
Vincent Laugier <vincent.laugier@gmail.com>
2023-10-15 20:11:01 +0000

    changes

--- src/Controller/MaintenanceCtrl/resetInfectiousStatusesOutbreaksAndExposures.jl
+++ src/Controller/MaintenanceCtrl/resetInfectiousStatusesOutbreaksAndExposures.jl
@@ -4,7 +4,7 @@ function MaintenanceCtrl.resetInfectiousStatusesOutbreaksAndExposures(
 
     # TODO: Add a security with a configuration to prevent invoking it by mistake
 
-    "DELETE FROM infectious_status" |>
+    "DELETE FROM infectious_status" |> # also deletes entries in 'event_requiring_attention'
     n -> PostgresORM.execute_plain_query(n,missing,dbconn)
 
     "DELETE FROM outbreak" |>
--- src/Model/InfectiousStatus.jl
+++ src/Model/InfectiousStatus.jl
@@ -3,13 +3,14 @@ mutable struct InfectiousStatus <: IInfectiousStatus
   patient::Union{Missing,Model.IPatient}
   contactExposure::Union{Missing,Model.IContactExposure}
   id::Union{Missing,String}
-  isCurrent::Union{Missing,Bool}
-  updatedRefTime::Union{Missing,ZonedDateTime}
-  infectiousStatus::Union{Missing,InfectiousStatusType.INFECTIOUS_STATUS_TYPE}
-  analysisRequestStatus::Union{Missing,AnalysisRequestStatusType.ANALYSIS_REQUEST_STATUS_TYPE}
   isConfirmed::Union{Missing,Bool}
   refTime::Union{Missing,ZonedDateTime}
   infectiousAgent::Union{Missing,InfectiousAgentCategory.INFECTIOUS_AGENT_CATEGORY}
+  analysisRequestStatus::Union{Missing,AnalysisRequestStatusType.ANALYSIS_REQUEST_STATUS_TYPE}
+  updatedRefTime::Union{Missing,ZonedDateTime}
+  isCancelled::Union{Missing,Bool}
+  isCurrent::Union{Missing,Bool}
+  infectiousStatus::Union{Missing,InfectiousStatusType.INFECTIOUS_STATUS_TYPE}
   outbreakInfectiousStatusAssoes::Union{Missing,Vector{Model.IOutbreakInfectiousStatusAsso}}
   eventRequiringAttentions::Union{Missing,Vector{Model.IEventRequiringAttention}}
 
@@ -18,27 +19,29 @@ mutable struct InfectiousStatus <: IInfectiousStatus
     patient = missing,
     contactExposure = missing,
     id = missing,
-    isCurrent = missing,
-    updatedRefTime = missing,
-    infectiousStatus = missing,
-    analysisRequestStatus = missing,
     isConfirmed = missing,
     refTime = missing,
     infectiousAgent = missing,
+    analysisRequestStatus = missing,
+    updatedRefTime = missing,
+    isCancelled = missing,
+    isCurrent = missing,
+    infectiousStatus = missing,
     outbreakInfectiousStatusAssoes = missing,
     eventRequiringAttentions = missing,
   ) = begin
-    x = new(missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,)
+    x = new(missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,missing,)
     x.patient = patient
     x.contactExposure = contactExposure
     x.id = id
-    x.isCurrent = isCurrent
-    x.updatedRefTime = updatedRefTime
-    x.infectiousStatus = infectiousStatus
-    x.analysisRequestStatus = analysisRequestStatus
     x.isConfirmed = isConfirmed
     x.refTime = refTime
     x.infectiousAgent = infectiousAgent
+    x.analysisRequestStatus = analysisRequestStatus
+    x.updatedRefTime = updatedRefTime
+    x.isCancelled = isCancelled
+    x.isCurrent = isCurrent
+    x.infectiousStatus = infectiousStatus
     x.outbreakInfectiousStatusAssoes = outbreakInfectiousStatusAssoes
     x.eventRequiringAttentions = eventRequiringAttentions
     return x
