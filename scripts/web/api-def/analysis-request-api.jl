29f97fbbe504dc3c60ce3eec6c6d8b1466cd367d
Vincent Laugier <vincent.laugier@gmail.com>
2023-10-23 05:04:45 +0000

    changes

--- scripts/web/api-def/analysis-request-api.jl
+++ scripts/web/api-def/analysis-request-api.jl
@@ -98,3 +98,108 @@ new_route = route("/api/analysis-request/save", req -> begin
 
 end)
 api_routes = (api_routes..., new_route) # append the route
+
+
+
+#
+# Listing of analyses
+#
+new_route = route("/api/analysis-request/listing", req -> begin
+
+    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    if req[:method] == "OPTIONS"
+        return(respFor_OPTIONS_req())
+    end
+
+    apiURL = "/api/analysis-request/listing"
+    @info "API $apiURL"
+    apiInTime = now(getTimeZone())
+
+    # Check if the user is allowed
+    status_code = TRAQUERUtil.initialize_http_response_status_code(req)
+
+    if status_code != 200
+        return Dict(:body => String(JSON.json(missing)),
+                    :headers => Dict("Content-Type" => "text/plain",
+                                      "Access-Control-Allow-Origin" => "*"),
+                    :status => status_code
+                     )
+    end
+
+    #
+    # Heart of the API
+    #
+
+    # Initialize results
+    error = nothing
+    appuser::Union{Nothing, Appuser} = nothing # Needs to be declared here to have it
+                                               # available in the catch block
+    query_result::Union{Dict,Missing} = missing
+
+    status_code = try
+
+        # Get the user as extracted from the JWT
+        appuser = req[:params][:appuser]
+
+        cryptPwd = TRAQUERUtil.extractCryptPwdFromHTTPHeader(req)
+
+        obj = JSON.parse(String(req[:data]))
+
+        # @info String(req[:data])
+        # Create the dictionary from the JSON
+        # serialize("tmp/json_str.jld",req[:data])
+
+        obj = PostgresORM.PostgresORMUtil.dictnothingvalues2missing(obj)
+        obj["pageNum"] += 1 # Pagination starts at 0 on the UI
+
+        query_result = AnalysisRequestCtrl.getAnalysesRequestsForListing(
+            obj["pageSize"],
+            obj["pageNum"],
+            obj["cols"]
+            ;cryptPwd = cryptPwd
+        )
+
+        # Log API usage
+        apiOutTime = now(getTimeZone())
+        WebApiUsageCtrl.logAPIUsage(
+            appuser,
+            apiURL,
+            apiInTime,
+            apiOutTime
+        )
+
+        200 # status code
+
+    catch e
+        formatExceptionAndStackTrace(e, stacktrace(catch_backtrace()), appuser)
+        # rethrow(e) # Do not rethrow the error because we do want to send a
+                     #  custom design if the file could not be retrieved
+        error = e
+        500 # status_code
+
+    end
+
+    #
+    # Prepare the result
+    #
+    result::Union{Missing,String} = missing
+    try
+        if status_code == 200
+            result = String(JSON.json(query_result)) # The client side doesn't really need the message
+        else
+            result = String(JSON.json(string(error)))
+        end
+    catch e
+        formatExceptionAndStackTrace(e, stacktrace(catch_backtrace()), appuser)
+        rethrow(e)
+    end
+
+    # Send the result
+    Dict(:body => result,
+         :headers => Dict("Content-Type" => "application/json",
+                          "Access-Control-Allow-Origin" => "*"),
+         :status => status_code
+        )
+
+end)
+api_routes = (api_routes..., new_route) # append the route
