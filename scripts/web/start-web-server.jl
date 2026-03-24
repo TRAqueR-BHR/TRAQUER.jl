21f46ff4830fae79034185fb4db03e9a3086a373
Vincent Laugier <vincent.laugier@gmail.com>
2026-03-24 06:26:34 +0000

    move main-web.jl to the deprecated folder

--- scripts/web/start-web-server.jl
+++ scripts/web/start-web-server.jl
@@ -1,2 +1,7 @@
 include("_prerequisite.jl")
 TRAQUER.WebAPI.serve()
+
+# The following is commented out because we want to have access to the REPL
+# Base.JLOptions().isinteractive == 0 && wait()
+
+TRAQUER.startScheduler()
