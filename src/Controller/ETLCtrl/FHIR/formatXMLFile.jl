5accb5e4c71fddfdf587d1270437cf5c28f08896
Vincent Laugier <vincent.laugier@gmail.com>
2026-04-13 14:29:09 +0000

    Implement temporary file handling in formatXMLFile function for safer XML formatting

--- src/Controller/ETLCtrl/FHIR/formatXMLFile.jl
+++ src/Controller/ETLCtrl/FHIR/formatXMLFile.jl
@@ -2,3 +2,14 @@ function ETLCtrl.FHIR.formatXMLFile(inputFilePath::String, outputFilePath::Strin
     cmd = `xmllint --format $inputFilePath -o $outputFilePath`
     run(cmd)
 end
+
+function ETLCtrl.FHIR.formatXMLFile(inputFilePath::String)
+    tmpFilePath = inputFilePath * ".tmp"
+    try
+        ETLCtrl.FHIR.formatXMLFile(inputFilePath, tmpFilePath)
+        mv(tmpFilePath, inputFilePath, force = true)
+    catch e
+        isfile(tmpFilePath) && rm(tmpFilePath)
+        rethrow(e)
+    end
+end
