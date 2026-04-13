bde8d41593f90b0a52594f6a5780faaa32e1a78b
Vincent Laugier <vincent.laugier@gmail.com>
2026-04-13 14:28:50 +0000

    Refactor test for convertExcelToFHIR to use joinpath for file paths

--- test/Controller/ETLCtrl/Excel/test-convertExcelToFHIR.jl
+++ test/Controller/ETLCtrl/Excel/test-convertExcelToFHIR.jl
@@ -1,12 +1,15 @@
 include("__prerequisite.jl")
 
 @testset "Test ETLCtrl.Excel.convertExcelToFHIR" begin
-    # Prepare test data
-    staysExcelFilePath = "custom/demo/test/sample-input-data/accidental_discovery_and_epidemic/demo-stays SALIOU.XLSX"
-    analysisExcelFilePath = "custom/demo/test/sample-input-data/accidental_discovery_and_epidemic/demo-analyses SALIOU.XLSX"
-    xmlOutputFilePath = "tmp/expected_fhir.xml"
 
-    # Call the function to be tested
-    fhir_output = ETLCtrl.Excel.convertExcelToFHIR(staysExcelFilePath, analysisExcelFilePath, xmlOutputFilePath)
+    rootDir = "custom/demo/test/sample-input-data/accidental_discovery_and_epidemic/"
+
+    # Declare the input stays and analyses Excel file paths and the output XML file path
+    staysExcelFilePath = joinpath(rootDir, "demo-stays SALIOU.XLSX")
+    analysisExcelFilePath = joinpath(rootDir, "demo-analyses SALIOU.XLSX")
+    xmlOutputFilePath = joinpath(rootDir, "demo-fhir SALIOU.xml")
+    fhir_output = ETLCtrl.Excel.convertExcelToFHIR(
+        staysExcelFilePath, analysisExcelFilePath, xmlOutputFilePath
+    )
 
 end
