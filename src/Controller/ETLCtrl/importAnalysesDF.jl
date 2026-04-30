cf729c620102fb0cf0b8af69351c3b219b2baf10
Vincent Laugier <vincent.laugier@gmail.com>
2026-04-30 05:11:25 +0000

    Add importAnalysesDF and importStaysDF functions for data integration

---
+++ src/Controller/ETLCtrl/importAnalysesDF.jl
@@ -0,0 +1,132 @@
+function ETLCtrl.importAnalysesDF(
+    df::DataFrame,
+    encryptionStr::AbstractString
+    ;stopAfterXLines::Number = Inf64,
+    ignoreEventsAfter::Union{Missing,ZonedDateTime} = missing
+)
+
+    @info (
+      "\n# #################################### #"
+    * "\n# Starting the integration of analyses #"
+    * "\n# #################################### #"
+    )
+
+    dbconn = TRAQUERUtil.openDBConn()
+    _tz = TRAQUERUtil.getTimeZone()
+
+    # Create an row number column so that we can go back to exact location in the source file
+    df.sourceRowNumber = 1:nrow(df)
+
+    @info "nrow(df) BEFORE filter[$(nrow(df))]"
+
+    # Group the rows on the analyis ref
+    # dfGoupedByAnalysisRef = groupby(df,:analysisRef)
+
+    try
+
+        counter = 1 # for debug
+
+        # for g in dfGoupedByAnalysisRef
+           # Get the first row
+        #    r = first(g)
+
+        for r in eachrow(df)
+
+            sourceRowNumber = r.sourceRowNumber
+
+            counter += 1 # for debug
+            if counter > stopAfterXLines # for debug
+              @info "We exit here because it was asked to stop after line[$stopAfterXLines]"
+              return # for debug
+            end # for debug
+
+            # Check if NIP_PATIENT is missing
+            if ismissing(r.patient_ref)
+               error("Error at line[$sourceRowNumber], patient_ref is missing")
+               continue
+            end
+
+            # Check if analysys ref is missing
+            if ismissing(r.analysis_ref)
+               error("Error at line[$sourceRowNumber], analysis_ref is missing")
+               continue
+            end
+
+
+            patientRef = r.patient_ref
+            analysisRef = r.analysis_ref
+            requestTime = r.request_time
+            resultTime = r.result_time
+            sample = r.sample
+            requestType = r.request_type
+            result = r.result
+
+            # We may want to simulate that we are at a given point in time, in which case
+            # some information need to be ignored
+            if !ismissing(ignoreEventsAfter)
+               if requestTime > ignoreEventsAfter
+                   continue
+               end
+               if (
+                  requestTime <= ignoreEventsAfter
+                  && !ismissing(resultTime)
+                  && resultTime > ignoreEventsAfter
+               )
+                  resultTime = missing
+               end
+           end
+
+            # Get a patient
+            patient =
+                PatientCtrl.retrieveOnePatient(patientRef, encryptionStr, dbconn)
+
+            if ismissing(patient)
+               errorMsg = ("Problem at line[$sourceRowNumber]."
+               * " Unable to find patient for ref[$patientRef]. "
+               * " Maybe a file of stays has not been integrated.")
+               @warn errorMsg
+               continue
+            end
+
+            # Get a stay
+            stay = StayCtrl.retrieveOneStayContainingDateTime(patient, requestTime, dbconn)
+
+            if ismissing(stay)
+                noStayErrorMsg = (
+                "Problem at row[$sourceRowNumber] of dataframe "
+                *"(line[$(sourceRowNumber+1)] of xlsx)."
+                * " Unable to find a stay for patient.id[$(patient.id)]"
+                * " patient.ref[$(patientRef)]"
+                * " starting before the date of the analyis request[$requestTime].")
+                @warn noStayErrorMsg
+                continue
+            end
+
+            analysisResult = AnalysisResult(
+               patient = patient,
+               stay = stay,
+               sampleMaterialType = sample,
+               requestTime = requestTime,
+               resultTime = resultTime,
+               result = result,
+               resultRawText = missing,
+               requestType = requestType,
+           )
+
+
+           analysisResult = AnalysisResultCtrl.upsert!(
+               analysisResult,
+               analysisRef,
+               encryptionStr,
+               dbconn
+           )
+
+        end # `for r in eachrow(df)`
+
+     catch e
+        rethrow(e)
+     finally
+        TRAQUERUtil.closeDBConn(dbconn)
+     end
+
+end
---
+++ src/Controller/ETLCtrl/importStaysDF.jl
@@ -0,0 +1,107 @@
+function ETLCtrl.importStaysDF(
+    df::DataFrame,
+    encryptionStr::AbstractString
+    ;ignoreEventsAfter::Union{Missing,ZonedDateTime} = missing
+)
+
+    @info (
+          "\n# ################################## #"
+        * "\n# Starting the integration of stays #"
+        * "\n# ################################## #"
+        )
+
+    dbconn = TRAQUERUtil.openDBConn()
+    _tz = TRAQUERUtil.getTimeZone()
+
+    counter = 0 # for debug
+    currentRowForDebug::Union{Missing,DataFrameRow} = missing
+    try
+
+        for r in eachrow(df)
+
+            counter += 1 # for debug
+            currentRowForDebug = r
+            @info "Treating line[$counter] of stays"
+            # if counter > 10 # for debug
+            #   TRAQUERUtil.commitDBTransaction(dbconn)
+            #   return # for debug
+            # end # for debug
+
+            unitCodeName = string(r.unit_code_name)
+            unitName = r.unit_name
+            ref = string(r.patient_ref)
+            firstname = string(r.firstname)
+            lastname = string(r.lastname)
+            birthdate::Date = r.birthdate
+
+            inTime =  r.unit_in_time isa ZonedDateTime ?
+                r.unit_in_time :
+                passmissing(ZonedDateTime(r.unit_in_time, getTimeZone()))
+            outTime = r.unit_out_time isa ZonedDateTime ?
+                r.unit_out_time :
+                passmissing(ZonedDateTime)(r.unit_out_time, getTimeZone())
+
+            hospitalizationInTime =  r.hospitalization_in_time isa ZonedDateTime ?
+                r.hospitalization_in_time :
+                passmissing(ZonedDateTime)(r.hospitalization_in_time, getTimeZone())
+            hospitalizationOutTime =  r.hospitalization_out_time isa ZonedDateTime ?
+                r.hospitalization_out_time :
+                passmissing(ZonedDateTime)(r.hospitalization_out_time, getTimeZone())
+
+            # We may want to simulate that we are at a given point in time, in which case
+            # some information need to be ignored
+            if !ismissing(ignoreEventsAfter)
+                if inTime > ignoreEventsAfter
+                    continue
+                end
+                if inTime <= ignoreEventsAfter && !ismissing(outTime) && outTime > ignoreEventsAfter
+                    outTime = missing
+                end
+                if !ismissing(hospitalizationOutTime) && hospitalizationOutTime > ignoreEventsAfter
+                    hospitalizationOutTime = missing
+                end
+            end
+
+            room = passmissing(string)(r.room)
+
+            # Get a unit
+            unit = UnitCtrl.createUnitIfNotExists(unitCodeName,unitName,dbconn)
+
+            # Get a patient
+            patient = PatientCtrl.createPatientIfNoExist(
+                firstname,
+                lastname,
+                birthdate,
+                ref,
+                encryptionStr,
+                dbconn
+            )
+
+            if ismissing(patient)
+                error("Unable to find patient for firstname[$firstname]"
+                * " lastname[$lastname] birthdate[$birthdateAsStr]."
+                * " Maybe a file of checks has not been integrated.")
+            end
+
+            # Retrieve the stay
+            stay = Stay(
+                patient = patient,
+                unit = unit,
+                inTime = inTime,
+                outTime = outTime,
+                hospitalizationInTime = hospitalizationInTime,
+                hospitalizationOutTime = hospitalizationOutTime,
+                room = room
+            )
+            StayCtrl.upsert!(stay, dbconn)
+
+        end # `for r in eachrow(df)`
+
+     catch e
+        @error "Problem at line $counter" currentRowForDebug
+        rethrow(e)
+     finally
+        TRAQUERUtil.closeDBConn(dbconn)
+     end
+
+end
