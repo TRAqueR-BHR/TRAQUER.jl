5263e2aa5802648738f7486ac164fb3a0847ccdd
Vincent Laugier <vincent.laugier@gmail.com>
2026-07-11 19:35:42 +0000

    add: expand comments and processing steps in downloadAndProcessFiles function

--- src/Controller/FileExchangeCtrl/downloadAndProcessFiles.jl
+++ src/Controller/FileExchangeCtrl/downloadAndProcessFiles.jl
@@ -5,7 +5,33 @@ function FileExchangeCtrl.downloadAndProcessFiles(
 )
 
     # Download files at given URL (it can be s3:// or file://)
+    # NOTE: If s3 uri then use S3Ctrl.download
+    # TODO
 
-    # Process them in order
+    # For each file xml file, download the sidecar file (name of sidecar file is same as xml
+    # file but with .sidecar extension).
+    # Create a vector of tuples (xmlFilePath, sideCarFilePath) for each file
+    # TODO
+
+
+    # Loop through the vector of tuples and process each xml file with its sidecar file to
+    # create a vector of decrypted files. The processing steps are:
+    # For each tuple (xmlFilePath, sideCarFilePath)
+    #  1. Extract the kdfChildKeyRef from the sidecar file using
+    #     FileExchangeCtrl.extractKdfChildKeyRefFromSidecarFile
+    #  2. Retrieve the KdfChildKey entity from the database using the kdfChildKeyRef
+    #     Use PostgresORM.retrieve_one_entity to retrieve the KdfChildKey entity from
+    #     the database using KdfChildKey(ref = childKeyRef)
+    #  3. Derive the child key hex using KdfChildKeyCtrl.deriveEncodedChildKey
+    #  4. Decrypt the file using FileExchangeCtrl.decryptFile with the derived child key hex
+    #     and add the decrypted file path to a vector of decrypted files
+
+    # Loop through the vector of decrypted files and process each decrypted file
+    # For each decrypted file:
+    #  1. Create a dataframe of analyses using ETLCtrl.FHIR.parseXMLToAnalysesDF
+    #  2. Create a dataframe of stays using ETLCtrl.FHIR.parseXMLToStaysDF
+    #  3. Import the analyses dataframe using ETLCtrl.importAnalysesDF
+    #  4. Import the stays dataframe using ETLCtrl.importStaysDF
+
 
 end
