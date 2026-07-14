24624349c16431803b13d5d4c398294b4f6d4bf1
Vincent Laugier <vincent.laugier@gmail.com>
2026-07-14 17:21:56 +0000

    fix: update comments for clarity in downloadAndProcessFile function

--- src/Controller/FileExchangeCtrl/downloadAndProcessFile.jl
+++ src/Controller/FileExchangeCtrl/downloadAndProcessFile.jl
@@ -5,7 +5,8 @@ function FileExchangeCtrl.downloadAndProcessFile(
 )
 
     # Download file at given URL (it can be s3:// or file://)
-    # NOTE: If s3 uri then use S3Ctrl.download
+    # NOTE: If s3 uri then use S3Ctrl.download ; if file uri move the file to
+    #       Conf.getFSProcessingInputFilesDir()
     # TODO
 
     # Download the sidecar file (name of sidecar file is same as xml file but with .sidecar
@@ -21,17 +22,17 @@ function FileExchangeCtrl.downloadAndProcessFile(
     #     the database using KdfChildKey(ref = childKeyRef)
     #  3. Derive the child key hex using KdfChildKeyCtrl.deriveEncodedChildKey
     #  4. Decrypt the file using FileExchangeCtrl.decryptFile with the derived child key hex
-    #     and add the decrypted file path to a vector of decrypted files
     # TODO
 
-    # Process the decrypted file:
+    # Process the decrypted file in a try-catch block
     #  1. Create a dataframe of analyses using ETLCtrl.FHIR.parseXMLToAnalysesDF
     #  2. Create a dataframe of stays using ETLCtrl.FHIR.parseXMLToStaysDF
     #  3. Import the analyses dataframe using ETLCtrl.importAnalysesDF
     #  4. Import the stays dataframe using ETLCtrl.importStaysDF
+    #  5. Move the processed files to the 'done' directory:
+    #     Conf.getFSDoneInputFilesDir()
+    #  NOTE
     # TODO
 
-    # Move the processed files to the processed directory
-    # TODO
 
 end
